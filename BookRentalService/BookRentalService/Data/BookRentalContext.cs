ce0042c79a47b357826d16d159c4401b3cc20a50
ParthiBanGitAcc <parbeece90@gmail.com>
2024-10-27 14:30:22 +0530

    Add files via upload
    code added

---
+++ BookRentalService/BookRentalService/Data/BookRentalContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookRentalService.Models;
+
+namespace BookRentalService.Data
+{
+    public class BookRentalContext : DbContext
+    {
+        public BookRentalContext(DbContextOptions<BookRentalContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+
+
+    }
+}
---
+++ BookRentalService/BookRentalService/Migrations/20241027031238_AddRentalCountToBooks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookRentalService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRentalCountToBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RentalCount",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RentalCount",
+                table: "Books");
+        }
+    }
+}
